e4f203a4573380694c501a7bdcd586f76bf4aafb
Igor Garcia <alexandre.igor64@gmail.com>
2020-06-01 20:38:48 -0300

    aula 08

---
+++ exercicio_4_2/Main.java
@@ -0,0 +1,11 @@
+package exercicio_4_2;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// Cria o cliente com o adaptador.
+		Cliente cliente = new Cliente(new SomadorAdapter());
+		cliente.executar();
+	}
+
+}
